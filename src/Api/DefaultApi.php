3cd78cb9c700c43af16a9cfb9d4a5fa54a662a70
Niko Granö <niko@fiteco.fi>
2021-08-04 10:32:41 +0300

    Use Guzzle's Psr7\QueryBuild instead of query_build (Vol 2)

--- src/Api/DefaultApi.php
+++ src/Api/DefaultApi.php
@@ -1182,7 +1182,7 @@ class DefaultApi
                 $httpBody = \GuzzleHttp\json_encode($formParams);
             } else {
                 // for HTTP post (form)
-                $httpBody = \GuzzleHttp\Psr7\build_query($formParams);
+                $httpBody = \GuzzleHttp\Psr7\Query::build($formParams);
             }
         }
 
@@ -1208,7 +1208,7 @@ class DefaultApi
             $headers
         );
 
-        $query = \GuzzleHttp\Psr7\build_query($queryParams);
+        $query = \GuzzleHttp\Psr7\Query::build($queryParams);
 
         return new Request(
             'GET',
